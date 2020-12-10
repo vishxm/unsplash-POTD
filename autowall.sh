229b407e8af1df769574d1970fbf08537bd0ad29
Vishwas Modhera <vmodhera7@gmail.com>
2020-12-10 19:58:11 +0530

    first commit

---
+++ autowall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python base.py
+feh --bg-fill image.png
+rm image.png
---
+++ base.py
@@ -0,0 +1,19 @@
+# libraries
+import requests
+from bs4 import BeautifulSoup
+
+# url to get the photo of the day id
+url = "https://unsplash.com/"
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
+photo_id = soup.find_all('a', '_2Mc8_ _3ZLHW _1HV49 _2WvKc')[0]['href'][8:]
+
+# url for getting the photo from unsplash API
+base_url = "http://source.unsplash.com/"
+photo_url = base_url + photo_id + '/1920x1080'
+
+# saving the photo 
+image = requests.get(photo_url)
+
+with open('image.png', 'wb') as f:
+    f.write(image.content)
