2e6a0dfbf15d80602e9a2f25cbaf985aef529abd
Vishwas Modhera <vmodhera7@gmail.com>
2020-12-12 15:12:41 +0530

    correcting path of image

--- autotheme.sh
+++ autotheme.sh
@@ -3,4 +3,4 @@ python base.py
 rm ~/.config/wpg/schemes/*image*.json
 wpg -a ~/us-scraper/image.png
 wpg -s ~/us-scraper/image.png
-feh --bg-fill image.png
+feh --bg-fill /home/vishwas/us-scraper/image.png
